a3d758180bd54cb18ee7b5c18ebaabc073d39b9e
Jacques Kvam <jacques@verdigris.co>
2015-09-20 15:02:20 -0700

    update to jupyter

--- 02-interactive.py
+++ 02-interactive.py
@@ -1,3 +1,3 @@
-from IPython.html.widgets import interact, interactive, fixed
-from IPython.html import widgets
+from ipywidgets import interact, interactive, fixed
+# from IPython.html import widgets
 from IPython.display import clear_output, display, HTML
